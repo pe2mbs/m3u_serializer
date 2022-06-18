960d1c6f895b7959fc77a20384289291de22c7f6
Marc Bertens - Administrator <m.bertens@pe2mbs.nl>
2022-06-18 16:30:36 +0200

    Updating so that the package is accepted by pypi.org

--- setup.py
+++ setup.py
@@ -15,13 +15,23 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
+import os
+import sys
 from distutils.core import setup
+__version__ = ''
+__author__ = ''
+# Get the root path of the project
+root_path = sys.argv[ 0 ].split( '/venv' )[ 0 ]
+# Read the __version__ and __author__ inforrmation
+with open( os.path.abspath( os.path.join( root_path, 'm3u_serializer', 'version.py' ) ) ) as f:
+       exec( f.read() )
+
+
 
 setup( name             = 'm3u_serializer',
-       version           = '0.1.0',
+       version           = __version__,
        description       = 'M3U serializer/deserializer',
-       author            = 'Marc Bertens-Nguyen',
+       author            = __author__,
        author_email      = 'm.bertens@pe2mbs.nl',
        url               = 'https://www.pe2mbs.nl/M3U_Serializer/',
        # packages          = [ 'm3u_serializer' ],
